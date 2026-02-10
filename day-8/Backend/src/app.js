1f9941af9183df6550d1f583ab13f88a9a8c13b2
Srijit Pradhan <srijitpradhan27@gmail.com>
2026-02-10 09:44:17 +0530

    feat: initialize frontend with React and Vite
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Created main application file (App.jsx) to display notes fetched from API.
    - Added CSS styles for the application layout and note components.
    - Configured Vite for development and build processes.
    - Included SVG assets for branding (Vite and React logos).

--- day-8/Backend/src/app.js
+++ day-8/Backend/src/app.js
@@ -1,8 +1,10 @@
 //server create
 const express = require("express");
 const noteModel = require("./models/note.model");
+const cors = require("cors");
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 //create a note and save in database
