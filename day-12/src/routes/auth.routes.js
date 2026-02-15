763c89d14d51b403196b4cc4ef3849541bd0ee29
Srijit Pradhan <srijitpradhan27@gmail.com>
2026-02-15 09:59:33 +0530

    in cookie token saved

--- day-12/src/routes/auth.routes.js
+++ day-12/src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const useModel = require("../models/user.model");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const authRoutes = express.Router();
 
@@ -16,7 +17,9 @@ authRoutes.post("/register", async (req, res) => {
     });
   }
 
-  const user = await userModel.create({ name, email, password });
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
+  const user = await userModel.create({ name, email, password:hash });
 
   const token = jwt.sign(
     {
@@ -26,10 +29,48 @@ authRoutes.post("/register", async (req, res) => {
     process.env.JWT_SECRET,
   );
 
-  res.cookie("jwt_token",token)
+  res.cookie("jwt_token", token);
 
   res.status(201).json({
     message: "User registered successfully",
+    user,
+    token,
+  });
+});
+
+authRoutes.post("/protected", (req, res) => {
+  console.log(req.cookies);
+});
+
+authRoutes.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const isPasswordValid = user.password === crypto.createHash("md5").update(password).digest("hex");
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
     user,
     token,
   });
